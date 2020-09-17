fe9a544d2fc683e15d35f309a2165dd8a8c876b5
Yuni CBM <yuni@u.northwestern.edu>
2020-09-17 16:31:34 -0500

    updated run_lda.py with simple nn decoder

---
+++ python/gpu.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+def set_gpu():
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    print("Num GPUs Available: ", len(physical_devices))
+    tf.config.experimental.set_memory_growth(physical_devices[0],True)
+
+    return
