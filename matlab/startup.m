b814124d8dbc56466d67892fbdab884b12ac2406
Yuni Teh <yuni@u.northwestern.edu>
2021-10-26 17:11:25 -0500

    done with most matlab parsing data

---
+++ matlab/startup.m
@@ -0,0 +1,10 @@
+set(0,'DefaultFigureWindowStyle','docked',...
+    'DefaultAxesFontName','Calibri',...
+    'DefaultTextFontName','Calibri')
+
+helper = genpath('C:\Users\yteh\Documents\work\git\projects\helper_functions');
+project = genpath('C:\Users\yteh\Documents\work\git\projects\latent_rep\matlab');
+
+addpath(helper, project);
+
+% clear;
