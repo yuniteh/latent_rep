387e35d715bfe01c86465aa017b9efbb7a181519
Yuni Teh <yuni@u.northwestern.edu>
2021-12-01 10:37:02 -0600

    done with stats_utils, no comments

--- python/stats_utils.py
+++ python/stats_utils.py
@@ -4,48 +4,6 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import pandas as pd
 
-def plot_fit(coef,T=1,Telec=1):
-    ## plot best fit line
-    x = np.linspace(0,5)
-    c = ['k','r','m']
-    c_tab = ['tab:purple','tab:blue', 'tab:orange', 'tab:purple','tab:blue', 'tab:orange','k','r','m']
-    c_i = 0
-    for n in [1,2,4,6,7,9,10,11,14]:
-        y = coef['Intercept']+ T[n-1] + coef['elec']*x + Telec[n-1]*x
-        plt.plot(x,y,color=c_tab[c_i])
-        c_i+=1
-    
-    return
-
-def get_coefs(mdf):
-    coef = mdf.params
-    i = 0
-    T = np.zeros(14,)
-    Telec = np.zeros(14,)
-    Telec2 = np.zeros(14,)
-    Telec3 = np.zeros(14,)
-    for ind in coef.index:
-        for iter in range(14):
-            if iter > 8:
-                st = 7
-            else:
-                st = 6
-            str_i = 'T.' + str(iter+1) + '.0]'
-            if ind[-st:] == str_i:
-                T[iter] = coef[i]
-            str_i = 'T.' + str(iter+1) + '.0]:elec'
-            if ind[-(st+5):] == str_i:
-                Telec[iter] = coef[i]
-            str_i = 'T.' + str(iter+1) + '.0]:elec2'
-            if ind[-(st+6):] == str_i:
-                Telec2[iter] = coef[i]
-            str_i = 'T.' + str(iter+1) + '.0]:elec3'
-            if ind[-(st+6):] == str_i:
-                Telec3[iter] = coef[i]
-        i +=1
-    
-    return T, Telec, Telec2, Telec3
-
 def create_dataframe(acc_clean,acc_noise):
     acc_clean[...,-1] = acc_clean[...,10]
     acc_clean = acc_clean[~np.isnan(acc_clean[:,0,0,0]),...]
@@ -77,3 +35,61 @@ def create_dataframe(acc_clean,acc_noise):
     df['elec4'] = df['elec']**4
 
     return df
+
+def get_mods(df):
+    if 0: # old models
+        out_df = df[(df['mod']!=0) & (df['mod']!=3)& (df['mod']!=5)& (df['mod']!=8)& (df['mod']!=12)& (df['mod']!=13)]
+    else:
+        out_df = df[(df['mod']==7) | (df['mod']==14)| (df['mod']==11)| (df['mod']==10)| (df['mod']==6)]
+
+    return out_df
+
+def run_fit(df,ctrl=10):
+    md = smf.mixedlm("acc ~ C(mod,Treatment(" + str(ctrl) + ")) + C(mod,Treatment(" + str(ctrl) + "))*elec", df,groups=df["sub"])
+
+    mdf = md.fit()
+    print(mdf.summary())
+
+    return mdf
+
+def get_coefs(mdf):
+    coef = mdf.params
+    i = 0
+    T = np.zeros(14,)
+    Telec = np.zeros(14,)
+    Telec2 = np.zeros(14,)
+    Telec3 = np.zeros(14,)
+    for ind in coef.index:
+        for iter in range(14):
+            if iter > 8:
+                st = 7
+            else:
+                st = 6
+            str_i = 'T.' + str(iter+1) + '.0]'
+            if ind[-st:] == str_i:
+                T[iter] = coef[i]
+            str_i = 'T.' + str(iter+1) + '.0]:elec'
+            if ind[-(st+5):] == str_i:
+                Telec[iter] = coef[i]
+            str_i = 'T.' + str(iter+1) + '.0]:elec2'
+            if ind[-(st+6):] == str_i:
+                Telec2[iter] = coef[i]
+            str_i = 'T.' + str(iter+1) + '.0]:elec3'
+            if ind[-(st+6):] == str_i:
+                Telec3[iter] = coef[i]
+        i +=1
+    
+    return T, Telec, Telec2, Telec3
+
+def plot_fit(coef,T=1,Telec=1):
+    ## plot best fit line
+    x = np.linspace(0,5)
+    c = ['k','r','m']
+    c_tab = ['tab:purple','tab:blue', 'tab:orange', 'tab:purple','tab:blue', 'tab:orange','k','r','m']
+    c_i = 0
+    for n in [1,2,4,6,7,9,10,11,14]:
+        y = coef['Intercept']+ T[n-1] + coef['elec']*x + Telec[n-1]*x
+        plt.plot(x,y,color=c_tab[c_i])
+        c_i+=1
+    
+    return
