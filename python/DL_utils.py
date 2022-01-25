fixed gradient tape, needed to add training=true, needs cleanup

python/DL_utils.py
@@ -122,8 +122,9 @@ def eval_nn(x, y, mod, clean):
 @tf.function
 def train_mlp(x, y, mlp, loss_fn, optimizer, train_loss, train_accuracy):
     with tf.GradientTape() as tape:
-        y_out = mlp(x)
-        loss = loss_fn(y, y_out)
+        y_out = mlp(x,training=True)
+        loss = tf.keras.losses.categorical_crossentropy(y,y_out)
+        # loss = loss_fn(y, y_out)
     gradients = tape.gradient(loss, mlp.trainable_variables)
     optimizer.apply_gradients(zip(gradients, mlp.trainable_variables))
 
@@ -141,8 +142,9 @@ def test_mlp(x, y, mlp, loss_fn, test_loss, test_accuracy):
 @tf.function
 def train_mlpbeta(x, y, mlp_beta, loss_fn, optimizer, train_loss, train_accuracy):
     with tf.GradientTape() as tape:
-        y_out = mlp_beta(x)
-        loss = loss_fn(y, y_out)
+        y_out = mlp_beta(x,training=True)
+        loss = tf.keras.losses.categorical_crossentropy(y,y_out)
+        # loss = loss_fn(y, y_out)
     gradients = tape.gradient(loss, mlp_beta.trainable_variables)
     optimizer.apply_gradients(zip(gradients, mlp_beta.trainable_variables))
 
@@ -160,8 +162,9 @@ def test_mlpbeta(x, y, mlp_beta, loss_fn, test_loss, test_accuracy):
 @tf.function
 def train_cnn(x, y, cnn, loss_fn, optimizer, train_loss, train_accuracy):
     with tf.GradientTape() as tape:
-        y_out = cnn(x)
-        loss = loss_fn(y, y_out)
+        y_out = cnn(x,training=True)
+        # loss = loss_fn(y, y_out)
+        loss = tf.keras.losses.categorical_crossentropy(y,y_out)
     gradients = tape.gradient(loss, cnn.trainable_variables)
     optimizer.apply_gradients(zip(gradients, cnn.trainable_variables))
 

python/process_data.py
@@ -104,10 +104,15 @@ def prep_train_data(d, raw, params):
     x_valid_noise_mlp = x_valid_noise_cnn.reshape(x_valid_noise_cnn.shape[0],-1)
 
     # create batches
-    trainmlp = tf.data.Dataset.from_tensor_slices((x_train_noise_mlp, y_train_clean)).batch(128)
-    validmlp = tf.data.Dataset.from_tensor_slices((x_valid_noise_mlp, y_valid_clean)).batch(128)
-    traincnn = tf.data.Dataset.from_tensor_slices((x_train_noise_cnn, y_train_clean)).batch(128)
-    validcnn = tf.data.Dataset.from_tensor_slices((x_valid_noise_cnn, y_valid_clean)).batch(128)
+    trainmlp = tf.data.Dataset.from_tensor_slices((x_train_noise_mlp, y_train_clean)).shuffle(x_train_noise_mlp.shape[0],reshuffle_each_iteration=True).batch(128)
+    validmlp = tf.data.Dataset.from_tensor_slices((x_valid_noise_mlp, y_valid_clean)).shuffle(x_train_noise_mlp.shape[0],reshuffle_each_iteration=True).batch(128)
+    traincnn = tf.data.Dataset.from_tensor_slices((x_train_noise_cnn, y_train_clean)).shuffle(x_train_noise_cnn.shape[0],reshuffle_each_iteration=True).batch(128)
+    validcnn = tf.data.Dataset.from_tensor_slices((x_valid_noise_cnn, y_valid_clean)).shuffle(x_train_noise_cnn.shape[0],reshuffle_each_iteration=True).batch(128)
+
+    # trainmlp = tf.data.Dataset.from_tensor_slices((x_train_noise_mlp, y_train_clean)).batch(128)
+    # validmlp = tf.data.Dataset.from_tensor_slices((x_valid_noise_mlp, y_valid_clean)).batch(128)
+    # traincnn = tf.data.Dataset.from_tensor_slices((x_train_noise_cnn, y_train_clean)).batch(128)
+    # validcnn = tf.data.Dataset.from_tensor_slices((x_valid_noise_cnn, y_valid_clean)).batch(128)
 
     d.emg_scale = emg_scale
     d.scaler = scaler