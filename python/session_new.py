6aa2f24e414cad62bf68c1e5ae4e50bd93865129
Yuni Teh <yuni@u.northwestern.edu>
2022-01-24 18:01:11 -0600

    cleanup done

--- python/session_new.py
+++ python/session_new.py
@@ -25,7 +25,7 @@ class Sess():
     def __init__(self,**settings):
         self.sub_type = settings.get('sub_type','AB')
         self.sub = settings.get('sub',1)
-        
+
         self.train_grp = settings.get('train_grp',2)
         self.train_scale = settings.get('train_scale',5)
         self.train = settings.get('train','fullallmix4')
@@ -42,51 +42,3 @@ class Sess():
     def update(self,**settings):
         for k in settings:
             setattr(self, k, settings[k])
-
-    def prep_train_data(self, raw, params, sub):
-        x_train, _, x_valid, p_train, _, p_valid = prd.train_data_split(raw,params,sub,self.sub_type,dt=self.cv_type,load=True,train_grp=self.train_grp)
-
-        emg_scale = np.ones((np.size(x_train,1),1))
-        for i in range(np.size(x_train,1)):
-            emg_scale[i] = 5/np.max(np.abs(x_train[:,i,:]))
-        x_train = x_train*emg_scale
-        x_valid = x_valid*emg_scale
-
-        x_train_noise, _, y_train_clean = prd.add_noise(x_train, p_train, sub, self.train, self.train_scale)
-        x_valid_noise, _, y_valid_clean = prd.add_noise(x_valid, p_valid, sub, self.train, self.train_scale)
-
-        # shuffle data to make even batches
-        x_train_noise, y_train_clean = shuffle(x_train_noise, y_train_clean, random_state = 0)
-
-        # Extract features
-        scaler = MinMaxScaler(feature_range=(0,1))
-        x_train_noise_cnn, scaler = prd.extract_scale(x_train_noise,scaler,self.scaler_load,ft=self.feat_type,emg_scale=emg_scale) 
-        x_valid_noise_cnn, _ = prd.extract_scale(x_valid_noise,scaler,ft=self.feat_type,emg_scale=emg_scale)
-        x_train_noise_cnn = x_train_noise_cnn.astype('float32')
-        x_valid_noise_cnn = x_valid_noise_cnn.astype('float32')
-
-        # reshape data for nonconvolutional network
-        x_train_noise_mlp = x_train_noise_cnn.reshape(x_train_noise_cnn.shape[0],-1)
-        x_valid_noise_mlp = x_valid_noise_cnn.reshape(x_valid_noise_cnn.shape[0],-1)
-
-        # create batches
-        trainmlp_ds = tf.data.Dataset.from_tensor_slices((x_train_noise_mlp, y_train_clean)).batch(128)
-        testmlp_ds = tf.data.Dataset.from_tensor_slices((x_valid_noise_mlp, y_valid_clean)).batch(128)
-        traincnn_ds = tf.data.Dataset.from_tensor_slices((x_train_noise_cnn, y_train_clean)).batch(128)
-        testcnn_ds = tf.data.Dataset.from_tensor_slices((x_valid_noise_cnn, y_valid_clean)).batch(128)
-
-        return trainmlp_ds, testmlp_ds, traincnn_ds, testcnn_ds, y_train_clean, y_valid_clean
-
-    def prep_test_data(self,raw,params,sub):
-        with open('real_noise/all_real_noise.p', 'rb') as f:
-            real_noise_temp, _ = pickle.load(f)
-
-        _, x_test, _, _, p_test, _ = prd.train_data_split(raw,params,sub,self.sub_type,dt=self.cv_type,train_grp=self.test_grp)
-        clean_size = int(np.size(x_test,axis=0))
-        x_test = x_test*emg_scale
-
-        x_test_noise, x_test_clean, y_test_clean = prd.add_noise(x_test, p_test, sub, self.test, 1, real_noise=real_noise_temp, emg_scale = emg_scale)
-        x_test_cnn, _ = prd.extract_scale(x_test_noise,scaler,ft=self.feat_type,emg_scale=emg_scale)
-        x_test_cnn = x_test_cnn.astype('float32')
-        x_test_mlp = x_test_cnn.reshape(x_test_cnn.shape[0],-1)
-
